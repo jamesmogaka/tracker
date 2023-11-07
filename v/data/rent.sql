5f12621252f1be540e5ff9f63a5078437a524a39
james mogaka <jamesoyondi23@gmail.com>
2023-11-07 13:57:12 +0300

    Finalised the rent query

--- v/data/rent.sql
+++ v/data/rent.sql
@@ -10,53 +10,65 @@ type rent = {
 */
 
 # Define the reporting period, i.e., month and year
-SET @report_month = 10;
-SET @report_year = 2023;
+set @report_month = 10;
+set @report_year = 2023;
 set @review_rate = 1.1;
 
-# Calculate the start and end dates of the reporting period based on the variables
-# @report_start_date-first day of the specified reporting month
-# calculate the @report_end_date as one month after the start date
-# SET @report_start_date()-gives this output YYYY-MM-01
-
-WITH
-   # current_report_date-This CTE creates a single-row result 
-   # containing the start date of the reporting period
-    current_report_date AS (
-        SELECT CONCAT(@report_year, '-', LPAD(@report_month, 2, '0'), '-01') AS value
+with
+    # Perfom a date calculation to establish the date of the previous period using
+    # the specified current period month and year. First using the month and year
+    # formulate a date for the current period then subtract a month from it to get
+    # the date of the previous period.
+    previous_period_date as (
+        select date_sub(CONCAT(@report_year, '-', LPAD(@report_month, 2, '0'), '-01'), interval 1 month) as `value`
     ),
-#
-# report-this CTE calculates current_date: The start date of the reporting period
-# and last_date: One month before the start date
-#
-# Read the reporting date from the period table ????????
-    report AS (
-        SELECT  
-            current_report_date.value AS `current_date`, 
-            date_sub(current_report_date.value, interval 1 month) AS last_date
-        FROM current_report_date   
+    #
+    # Retrive the start date of a given period 
+    # Get the date of the previous period from the cte above and use the year and month
+    # to get the cutoff of the previous period as the start date of the current period
+    initial_date as(
+        select 
+            cutoff as start_date
+        from 
+            `period` join previous_period_date
+        where 
+            year = year(`value`) and month = month(`value`)
     ),
-#
-# Get the client infomation as it from the database
-# This is helpful in calculation of the current rent e.g establishing the method
-# in which the client pays the rent either quaterly or monthly 
-# The infomation of the client is also useful in reporting
-    client AS (
-        SELECT
+    #
+    # Read the reporting date from the period table
+    # The start date of the report is gotten from the cutoff of the immediate period before
+    # the reporting period and the end date is the cutoff of the period specified by a matchin
+    # year and month as the report_year and report_month specified 
+    # The end date is simply fetched directly as it is from the period table but the start_date
+    # we must calculate the month and even the year of the period just before the reporting period
+    report as (
+        select  
+            initial_date.start_date as genesis,
+            (select cutoff from `period` where year = @report_year and month = @report_month)as last_date
+        from
+            initial_date
+    ),
+    #
+    # Get the client infomation as it from the database
+    # This is helpful in calculation of the current rent e.g establishing the method
+    # in which the client pays the rent either quaterly or monthly 
+    # The infomation of the client is also useful in reporting
+    client as (
+        select
             client,
-            name,
+            `name`,
             quarterly
-        FROM
+        from
             client
     ),
-#
-# Get all valid agreement details that will be help in calculation of rent, i.e.,
-# all valid contracts that have not reached termination
-# Here we will get the start date of the contract the end date the acctual room 
-# that is rented and based on this infomation we can calculate the correct amount
-# of rent before and after the review
-    agreement AS (
-        SELECT 
+    #
+    # Get all valid agreement details that will be help in calculation of rent, i.e.,
+    # all valid contracts that have not reached termination
+    # Here we will get the start date of the contract the end date the acctual room 
+    # that is rented and based on this infomation we can calculate the correct amount
+    # of rent before and after the review
+    agreement as (
+        select 
             agreement,
             client,
             room,
@@ -65,34 +77,36 @@ WITH
             `terminated`,
             duration,
             review
-        FROM
+        from
             agreement
-        WHERE
-            valid = 1 AND `terminated` IS NULL
+        where
+            valid = 1 and `terminated` is null
     ),
-#
-# Calculate the date of termination and review of each contract that was retrived. 
-# This is to check if a client's ammount should be raised due to contract extension or review. 
-# Both the termination and review dates are crucial in calculation of the unit amount of a room
-    termination AS (
-        SELECT
+    #
+    # Calculate the date of termination and review of each contract that was retrived. 
+    # This is to check if a client's ammount should be raised due to contract extension or review. 
+    # Both the termination and review dates are crucial in calculation of the unit amount of a room
+    termination as (
+        select
             agreement,
             client,
             room,
             amount,
             start_date,
-            DATE_ADD(start_date, INTERVAL duration YEAR) AS `terminated`,
-            DATE_ADD(start_date, INTERVAL review YEAR) AS review
-        FROM
+            `terminated`,
+            review,
+            DATE_ADD(start_date, interval duration year) as `termination_date`,
+            DATE_ADD(start_date, interval review year) as review_date
+        from
             agreement
     ),
-#
-# Perfom the review based on termination
-# get all the agreements whoes termination date is elapsed by the report last date
-# and apply the termination review ratio to the ammount to get a more upto date 
-# rent ammount
-    termination_review AS (
-        SELECT
+    #
+    # Perfom the review based on termination
+    # get all the agreements whoes termination date is elapsed by the report last date
+    # and apply the termination review ratio to the ammount to get a more upto date 
+    # rent ammount
+    termination_review as (
+        select
             agreement,
             client,
             room,
@@ -100,58 +114,71 @@ WITH
             `terminated`,
             review,
             amount as initial_amount,
-            CASE 
-                WHEN `terminated` < last_date THEN amount * @review_rate
-                ELSE amount
-            END AS amount
-        FROM
+            genesis, 
+            last_date,
+            case 
+                when `terminated` < last_date then amount * @review_rate
+                else amount
+            end as amount
+        from
             termination join report
     ),
-#
-# We need also to calculate the new unit rent after the review period has elapsed
+    #
+    # We need also to calculate the new unit rent after the review period has elapsed
 
-#
-# For clients with quaterly rent we need to calculate based on their contract
-# start date when does their quater end inorder to invoice them 3 times the monthly
-# amount to get the rent for the quater
-    RECURSIVE quarter AS(
-        SELECT 
-            termination_review.client,
-            1 AS quarter, 
-            DATE_ADD(termination_review.start_date, INTERVAL 3 MONTH) AS quarter_date
-        FROM
-            termination_review
-            INNER JOIN client ON termination_review.client = client.client
-        WHERE
-            client.quaterly = 1 
-        UNION ALL
-        SELECT 
-            quarter.client,
-            quarter.quarter + 1, 
-            DATE_ADD(quarter.quarter_date, INTERVAL 3 MONTH)
-        FROM 
-            quarter 
-        WHERE quarter.quarter < 4
-    ),
-#
-# After all reviews are alredy taken care of we now need to use the calculated
-# amount to figure out the rent of the curent reporting period
-    rent AS (
-        SELECT
-            client.*,
+
+    #
+    # After all reviews are alredy taken care of we now need to use the calculated
+    # amount to figure out the rent of the curent reporting period
+    # There are two types of clients those who pay on a monthly basis and a quarterly basis
+    # For the clients who pay on a monthly basis their rent will always remain
+    # the same as the amount while for the clients who pay on a quarterly basis
+    # the rent is charged after every 3 months and it will is thrice the amount once
+    # it is charged
+    rent as (
+        select
+            client.client,
+            client.`name`,
+            client.quarterly,
             termination_review.room,
             termination_review.start_date,
             termination_review.amount,
-            CASE
-                WHEN client.quaterly = 0 THEN amount
-                ELSE quaterly_rent.rent_due
-            END AS rent_due
-        FROM
+            case
+                when client.quarterly = 0 then amount
+                else if(timestampdiff(month, termination_review.last_date,termination_review.start_date) mod 3 = 0, termination_review.amount * 3, 0)
+            end as rent_due
+        from
             termination_review
-            INNER JOIN client on termination_review.client = client.client
-            # join the quaterly_rent cte !!!!
+            inner join client on termination_review.client = client.client
+    ),
+    #
+    # Using the above claculations generate the appropriate monthly report
+    # The reporting output is of the following type :-
+    /*
+        type report = {
+            client: number,
+            quterly: boolean,
+            details: {room:number, amount:number, rent_due:number},
+            total_rent_due: number
+        }
+    */
+    rent_report as(
+        select
+            rent.client,
+            rent.quarterly,
+            json_arrayagg(
+                json_object(
+                    'room', rent.room, 
+                    'amount', rent.amount,
+                    'rent_due', rent.rent_due
+                )
+            ) as details,
+            sum(rent.rent_due) as total_rent_due
+        from
+            rent
+        group by rent.client,rent.quarterly
     )
 
 
 
-select * from quarter;
+select * from rent_report;
